50fc60fd9c7a41086a5c014d2083f541fe7e1b7b
Dennis Musembi <dennismusembi344@gmail.com>
2023-03-22 18:37:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+//function filter(driver,name){
+    //let newDriver=[]
+    //for(let user of driver){
+        //if(name(user)){
+        //    newDriver.push(user)
+      //  }
+    //}
+  //  return newDriver
+//}
+ function findMatching(drivers,name){
+    return drivers.filter(driver=>(driver.toLowerCase()===name.toLowerCase()))
+
+ }
+ function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  
