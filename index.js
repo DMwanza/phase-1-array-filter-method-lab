f4adf88034c5f098d44ee0c4f800c91cd1293155
Dennis <dennismusembi344@gmail.com>
2024-02-19 13:31:32 +0300

    done2

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 // Code your solution here
-const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 //function filter(driver,name){
-    //let newDriver=[]
-    //for(let user of driver){
-        //if(name(user)){
-        //    newDriver.push(user)
+  //let newDriver=[]
+  //for(let user of driver){
+    //if(name(user)){
+      //    newDriver.push(user)
       //  }
-    //}
-  //  return newDriver
-//}
- function findMatching(drivers,name){
-    return drivers.filter(driver=>(driver.toLowerCase()===name.toLowerCase()))
+      //}
+      //  return newDriver
+      //}
+      //  function findMatching(drivers,name){
+        //     return drivers.filter(driver=>(driver.toLowerCase()===name.toLowerCase()))
+        
+        //  }
+        //  function fuzzyMatch(drivers, letters) {
+          //     return drivers.filter(driver => driver.startsWith(letters));
+          //   }
+          
+          //   function matchName(drivers, name) {
+//     return drivers.filter(driver => driver.name === name);
+//   }
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+  const findMatching=(array,name)=>{
+    return array.filter(user=>user.toLowerCase()===name.toLowerCase())
 
- }
- function fuzzyMatch(drivers, letters) {
-    return drivers.filter(driver => driver.startsWith(letters));
   }
-
-  function matchName(drivers, name) {
-    return drivers.filter(driver => driver.name === name);
+  const fuzzyMatch=(array,name)=>{
+    return array.filter(user=>user.startsWith(name))
   }
-  
+  const matchName=(array,name)=>{
+    return array.filter(user=>user.name===name)
+  }
   
